7379eab73cb5fe85e65daefe7895d63784d5315e
jihunJeong <jihunjeong@gmail.com>
2020-05-21 21:29:17 +0900

    init

---
+++ FreshFood/FreshFood/FoodList/FoodListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodListTableViewCell.swift
+//  FreshFood
+//
+//  Created by 정지훈 on 2020/05/21.
+//  Copyright © 2020 정지훈. All rights reserved.
+//
+
+import UIKit
+
+class FoodListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ FreshFood/FreshFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreshFood
+//
+//  Created by 정지훈 on 2020/05/21.
+//  Copyright © 2020 정지훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
