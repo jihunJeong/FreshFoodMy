1f411308159288ecd01b49202eb5e8964fce9634
jihunJeong <jihun.jeong219@gmail.com>
2020-05-29 10:10:44 +0900

    customize TableView

---
+++ FreshFood/FreshFood/FoodList/detailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  detailViewController.swift
+//  FreshFood
+//
+//  Created by 정지훈 on 2020/05/28.
+//  Copyright © 2020 정지훈. All rights reserved.
+//
+
+import UIKit
+
+class detailViewController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
